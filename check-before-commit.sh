472d7fd3e0e7a6a77f93378cda81c61e938f0707
kpe <kpe.git@gmailbox.org>
2019-11-18 12:26:17 +0100

    to/from file json serialization added

--- check-before-commit.sh
+++ check-before-commit.sh
@@ -2,7 +2,7 @@
 
 PEP8_IGNORE=E221,E501,W504,W391
 
-pep8 --ignore=${PEP8_IGNORE} --exclude=tests,.venv -r --show-source tests params
+pycodestyle --ignore=${PEP8_IGNORE} --exclude=tests,.venv -r --show-source tests params
 
 coverage run --source=params $(which nosetests) --with-doctest tests/
 coverage report --show-missing --fail-under=100
